25f89f2be3b04268edee324e150f952215ae28c7
MohGumaa <gomaam14@gmail.com>
2025-06-29 17:52:49 +0400

    setup project files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<h1 className="font-medium">
+			Buy New &amp; Used Honda Cars - Sedans and SUVs | Honda
+		</h1>
+	);
+};
+
+export default App;
