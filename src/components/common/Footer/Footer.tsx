0aba50e72c8745df72f3363807d81b44cc1a8cd3
MohGumaa <gomaam14@gmail.com>
2025-07-02 22:34:59 +0400

    customize theme

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer className="container mx-auto py-4 container-padding">
+			<p>Footer</p>
+		</footer>
+	);
 };
 
 export default Footer;
