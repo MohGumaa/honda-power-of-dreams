5806d079a6111c3d160139b35dac7d9b33e5c67c
MohGuma <gomaam14@gamil.com>
2025-07-10 18:29:46 +0400

    added the project structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header"
+
+const App = () => {
+  return (
+   <>
+    <Header/>
+    <main className="min-h-screen">MAin</main>
+   </>
+  )
+}
+
+export default App
---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+// import type { NavLink } from '../types';
+
+export const NavLinks = [
+	{ id: 0, text: 'models', href: '#' },
+	{ id: 1, text: 'features', href: '#' },
+	{ id: 2, text: 'technology', href: '#' },
+	{ id: 3, text: 'contact', href: '#' },
+];
